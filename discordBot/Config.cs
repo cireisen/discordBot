a15160f7786e0853e2884c4a7c2758e80ed9e8cc
cireisen <kdh_214@naver.com>
2023-03-17 11:59:58 +0900

    Update Config.cs

--- discordBot/Config.cs
+++ discordBot/Config.cs
@@ -8,7 +8,7 @@ namespace discordBot
 {
     internal static class Config
     {
-        public static string Token = "MTA3NTkzNjg4MDg1MzUyMDQzNA.GYgWV4.pE6A0dPRzBi0DKzGE8CZw2bbCn_BqAfVmMu8vY";
+        public static string Token = "";
         public static ulong ControlGuildID = 845134635725225994;
         public static ulong ControlChannelID = 845134635725225997;
     }
