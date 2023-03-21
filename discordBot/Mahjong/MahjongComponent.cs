765085d2a7c6ad190753d80f49a1d51dfc4f4276
kdh_214@naver.com <pooprozer@ack.co.kr>
2023-03-21 18:05:38 +0900

    메시지 붙이는 내용 공통 함수들 추가

---
+++ discordBot/Mahjong/MahjongComponent.cs
@@ -0,0 +1,155 @@
+﻿using Discord;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discordBot.Mahjong
+{
+    internal class MahjongComponent
+    {
+        
+
+        public static EmbedBuilder CreateMahjongMainEmbed()
+        {
+            return new EmbedBuilder()
+            {
+                Title = "마작 어시스터",
+                ThumbnailUrl = @"https://pbs.twimg.com/profile_images/1118736457554964480/_G7Au64E_400x400.png"
+            };
+        }
+
+        public static (Embed embed, MessageComponent component) CreateMainGameMsg(ulong gameHandler)
+        {
+            string filePath = Config.path + @$"mahjong\{gameHandler}.json";
+
+            int playerCount = 0;
+
+            string ton = "";
+            string nan = "";
+            string sha = "";
+            string pe = "";
+
+            string wind = "";
+            string rounds = "";
+            string extra = "";
+
+            string gameType = "";
+            string table = "";
+            using (StreamReader file = File.OpenText(filePath))
+            {
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+
+                    JObject json = (JObject)JToken.ReadFrom(reader);
+                    try
+                    {
+                        playerCount = (int)json["PlayerCount"];
+
+                        ton = json["Ton"].ToString();
+                        nan = json["Nan"].ToString();
+                        sha = json["Sha"].ToString();
+                        pe = json["Pe"].ToString();
+
+                        rounds = json["Rounds"].ToString();
+                        gameType = json["GameType"].ToString();
+                        if (gameType == "Han")
+                        {
+                            gameType = "반장전";
+                        }
+                        else if (gameType == "Ton")
+                        {
+                            gameType = "동풍전";
+                        }
+
+                        extra = json["Extra"].ToString();
+                        table = json["Table"].ToString();
+
+                        switch (json["Wind"].ToString())
+                        {
+                            case "Ton":
+                                wind = "동풍";
+                                break;
+                            case "Nan":
+                                wind = "남풍";
+                                break;
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+
+            var fieldTon = new EmbedFieldBuilder()
+                .WithName("동")
+                .WithValue(ton)
+                .WithIsInline(true);
+            var fieldNan = new EmbedFieldBuilder()
+                 .WithName("남")
+                 .WithValue(ton)
+                 .WithIsInline(true);
+            var fieldTable = new EmbedFieldBuilder()
+                .WithName($"{extra}연짱")
+                .WithValue($"공탁{table}점")
+                .WithIsInline(false);
+            var fieldSha = new EmbedFieldBuilder()
+                .WithName("서")
+                .WithValue(ton)
+                .WithIsInline(true);
+            var fieldPe = new EmbedFieldBuilder()
+                .WithName("북")
+                .WithValue(ton)
+                .WithIsInline(true);
+
+            EmbedBuilder embedBuilder = CreateMahjongMainEmbed()
+                .WithDescription($"{gameType} {wind} {rounds}국")
+                .AddField(fieldTon)
+                .AddField(fieldNan)
+                .AddField(fieldTable)
+                .AddField(fieldSha);
+
+            if(playerCount > 3)
+            {
+                embedBuilder.AddField(fieldPe);
+            }
+
+            MessageComponent component = CreateMainGameComponent(playerCount);
+
+            return (embedBuilder.Build(), component);
+        }
+
+        public static MessageComponent CreateMainGameComponent(int playerCount)
+        {
+            ComponentBuilder builder = new ComponentBuilder()
+                .WithButton("동", "mahj_ton")
+                .WithButton("남", "mahj_nan")
+                .WithButton("서", "mahj_sha");
+
+            if(playerCount > 3)
+            {
+                builder.WithButton("북", "mahj_pe");
+            }
+
+            return builder.Build();
+        }
+
+        public static MessageComponent CreateUserComponent(string wind)
+        {
+            return new ComponentBuilder().WithButton("asdf", "").Build();
+        }
+
+        private static ActionRowBuilder CreateBackwardComponent()
+        {
+            ComponentBuilder builder = new ComponentBuilder()
+                .WithButton("뒤로가기", "mahj_back")
+                .WithButton("처음으로", "mahj_first");
+            
+            return builder.ActionRows.First();
+        }
+    }
+}
--- discordBot/Mahjong/MahjongMain.cs
+++ discordBot/Mahjong/MahjongMain.cs
@@ -16,18 +16,6 @@ namespace discordBot.Mahjong
 {
     internal class MahjongMain
     {
-        #region Private
-
-        private static EmbedBuilder CreateMahjongMainEmbed()
-        {
-            return  new EmbedBuilder()
-            {
-                Title = "마작 어시스터",
-                ThumbnailUrl = @"https://pbs.twimg.com/profile_images/1118736457554964480/_G7Au64E_400x400.png"
-            };
-        }
-
-        #endregion
 
         public static async Task ReadCommand(SocketInteraction socket, string id)
         {
@@ -44,7 +32,7 @@ namespace discordBot.Mahjong
 
         public static async Task ShowMain(SocketInteraction command)
         {
-            var embed = CreateMahjongMainEmbed()
+            var embed = MahjongComponent.CreateMahjongMainEmbed()
                 .WithDescription("아직 개발중");
 
             embed.AddField("원하시는 기능의 버튼을 클릭해 주세요", "응애");
@@ -86,113 +74,36 @@ namespace discordBot.Mahjong
                 long count = (long)slash.Data.Options.First().Value;
 
                 CreateGame(userId, count);
-
-                await command.RespondAsync("", embed:CreateMainGameEmbed(userId), ephemeral: false);
+                var messageInfo = MahjongComponent.CreateMainGameMsg(userId);
+                await command.RespondAsync("", embed: messageInfo.embed, ephemeral: false, components: messageInfo.component);
             }
         }
 
-        public static Embed CreateMainGameEmbed(ulong gameHandler)
+        public static async Task StartGame(SocketInteraction command, long count)
         {
-            string filePath = Config.path + @$"mahjong\{gameHandler}.json";
-
-            int playerCount;
-
-            string ton = "";
-            string nan = "";
-            string sha = "";
-            string pe = "";
-
-            string wind = "";
-            string rounds = "";
-            string extra = "";
-
-            string gameType = "";
-            string table = "";
-            using (StreamReader file = File.OpenText(filePath))
+            ulong userId = command.User.Id;
+            if (command.GetType() == typeof(SocketMessageComponent))
             {
-                using (JsonTextReader reader = new JsonTextReader(file))
+                SocketMessageComponent msg = (SocketMessageComponent)command;
+                await msg.UpdateAsync(x =>
                 {
-
-                    JObject json = (JObject)JToken.ReadFrom(reader);
-                    try
-                    {
-                        playerCount = (int)json["PlayerCount"];
-
-                        ton = json["Ton"].ToString();
-                        nan = json["Nan"].ToString();
-                        sha = json["Sha"].ToString();
-                        pe  = json["Pe"].ToString();
-
-                        rounds = json["Rounds"].ToString();
-                        gameType = json["GameType"].ToString();
-                        if(gameType == "Han")
-                        {
-                            gameType = "반장전";
-                        }
-                        else if(gameType == "Ton")
-                        {
-                            gameType = "동풍전";
-                        }
-
-                        extra = json["Extra"].ToString();
-                        table = json["Table"].ToString();
-                        switch (json["Wind"].ToString())
-                        {
-                            case "Ton":
-                                wind = "동풍";
-                                break;
-                            case "Nan":
-                                wind = "남풍";
-                                break;
-                        }
-                    }
-                    catch
-                    {
-
-                    }
-                }
+                    //x.Embed = embed.Build();
+                    //x.Components = builder.Build();
+                });
             }
+            else if (command.GetType() == typeof(SocketSlashCommand))
+            {
+                SocketSlashCommand slash = (SocketSlashCommand)command;
 
-            var fieldTon = new EmbedFieldBuilder()
-                .WithName("동")
-                .WithValue(ton)
-                .WithIsInline(true);
-            var fieldNan = new EmbedFieldBuilder()
-                 .WithName("남")
-                 .WithValue(ton)
-                 .WithIsInline(true);
-            var fieldTable = new EmbedFieldBuilder()
-                .WithName($"{extra}연짱")
-                .WithValue($"공탁{table}점")
-                .WithIsInline(false);
-            var fieldSha = new EmbedFieldBuilder()
-                .WithName("서")
-                .WithValue(ton)
-                .WithIsInline(true);
-            var fieldPe = new EmbedFieldBuilder()
-                .WithName("북")
-                .WithValue(ton)
-                .WithIsInline(true);
+                CreateGame(userId, count);
 
-            //EmbedBuilder embedBuilder = new EmbedBuilder()
-            //{
-            //    Title = "마작 어시스터",
-            //    ThumbnailUrl = @"https://pbs.twimg.com/profile_images/1118736457554964480/_G7Au64E_400x400.png",
-            //    Fields = new List<EmbedFieldBuilder> { fieldTon, fieldPe, fieldTable, fieldNan, fieldSha }
+                var gameinfo = MahjongComponent.CreateMainGameMsg(userId);
 
-            //};
-
-            EmbedBuilder embedBuilder = CreateMahjongMainEmbed()
-                .WithDescription($"{gameType} {wind} {rounds}국")
-                .AddField(fieldTon)
-                .AddField(fieldNan)
-                .AddField(fieldTable)
-                .AddField(fieldSha)
-                .AddField(fieldPe)
-                .WithFooter($"");
-
-            return embedBuilder.Build();
+                await command.RespondAsync("", embed: gameinfo.embed, ephemeral: false, components: gameinfo.component);
+            }
         }
+
+        
 
         private static void CreateGame(ulong gameHandler, long playerCount)
         {
